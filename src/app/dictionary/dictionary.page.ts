edb425b619d0020f732776918cd5a23679f5f04d
totally real name <totally real email>
2025-04-25 04:44:51 +0100

    add search bar on dictionary page

--- src/app/dictionary/dictionary.page.ts
+++ src/app/dictionary/dictionary.page.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton} from '@ionic/angular/standalone';
 import { HttpClient } from '@angular/common/http';
 import { DictionaryEntry } from '../dictionary-types';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-dictionary',
 	templateUrl: './dictionary.page.html',
 	styleUrls: ['./dictionary.page.scss'],
 	standalone: true,
-	imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+	imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonButton, CommonModule, FormsModule]
 })
 export class DictionaryPage implements OnInit
 {
+	public searchText: string = "";
 	public entry: DictionaryEntry | null = null;
 
-	constructor(private route: ActivatedRoute, private http: HttpClient) {}
+	constructor(private router: Router, private route: ActivatedRoute, private http: HttpClient) {}
 
 	ngOnInit()
 	{
@@ -30,5 +32,10 @@ export class DictionaryPage implements OnInit
 			{
 			}
 		});
+	}
+
+	search()
+	{
+		this.router.navigateByUrl(`/dictionary/${this.searchText}`, { replaceUrl: true });
 	}
 }
