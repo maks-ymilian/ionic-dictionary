91144eb9d3a5a1b0b5cd6d27203aef43ec0e1c07
totally real name <totally real email>
2025-04-25 11:57:01 +0100

    make history work

--- src/app/dictionary/dictionary.page.ts
+++ src/app/dictionary/dictionary.page.ts
@@ -6,6 +6,7 @@ import { HttpClient } from '@angular/common/http';
 import { DictionaryEntry } from '../dictionary-types';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
+import { HistoryService } from '../history.service'
 
 @Component({
 	selector: 'app-dictionary',
@@ -19,19 +20,22 @@ export class DictionaryPage implements OnInit
 	public searchText: string = "";
 	public entry: DictionaryEntry | null = null;
 
-	constructor(private router: Router, private route: ActivatedRoute, private http: HttpClient) {}
+	constructor(private router: Router, private route: ActivatedRoute, private http: HttpClient, public history: HistoryService) {}
 
 	ngOnInit()
 	{
 		const word: string | null = this.route.snapshot.paramMap.get('word');
-		this.http.get<DictionaryEntry[]>("https://api.dictionaryapi.dev/api/v2/entries/en/" + word).subscribe(res =>
+		if (word !== null)
 		{
-			if (res.length != 0)
-				this.entry = res[0];
-			else
+			this.http.get<DictionaryEntry[]>("https://api.dictionaryapi.dev/api/v2/entries/en/" + word).subscribe(res =>
 			{
-			}
-		});
+				if (res.length != 0)
+				{
+					this.entry = res[0];
+					this.history.addEntry(word);
+				}
+			});
+		}
 	}
 
 	search()
--- src/app/history/history.page.ts
+++ src/app/history/history.page.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton} from '@ionic/angular/standalone';
+import { HistoryService } from '../history.service'
 
 @Component({
 	selector: 'app-history',
 	templateUrl: './history.page.html',
 	styleUrls: ['./history.page.scss'],
 	standalone: true,
-	imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+	imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
 })
 export class HistoryPage implements OnInit
 {
-	constructor() {}
+	constructor(public history: HistoryService) {}
 
-	ngOnInit()
+	ngOnInit() {}
+
+	clear()
 	{
+		this.history.clear();
 	}
 }
