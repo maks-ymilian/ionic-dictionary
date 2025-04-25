1d51c786340988c7bd6d930e0dc342e3530d40b1
totally real name <totally real email>
2025-04-25 12:23:13 +0100

    copy definition to clipboard

--- src/app/dictionary/dictionary.page.ts
+++ src/app/dictionary/dictionary.page.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton} from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonText } from '@ionic/angular/standalone';
 import { HttpClient } from '@angular/common/http';
 import { DictionaryEntry } from '../dictionary-types';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { HistoryService } from '../history.service'
+import { Clipboard } from '@capacitor/clipboard';
 
 @Component({
 	selector: 'app-dictionary',
 	templateUrl: './dictionary.page.html',
 	styleUrls: ['./dictionary.page.scss'],
 	standalone: true,
-	imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonButton, CommonModule, FormsModule]
+	imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonButton, CommonModule, FormsModule, IonText]
 })
 export class DictionaryPage implements OnInit
 {
@@ -41,5 +42,10 @@ export class DictionaryPage implements OnInit
 	search()
 	{
 		this.router.navigateByUrl(`/dictionary/${this.searchText}`, { replaceUrl: true });
+	}
+
+	copy(string: string)
+	{
+		Clipboard.write({ string: string });
 	}
 }
